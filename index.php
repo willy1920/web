59fbd58d063a290d6421f92003aac3aa0b17c828
Willy <willychai05@gmail.com>
2017-09-04 21:19:51 +0700

    init

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Tunas Bangsa</title>
+	<link rel="stylesheet" href="style/style.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+	<img src="icon/menuIcon.png" alt="Menu alt" id="menuIcon">
+	<section id="leftMenu">
+		<div class="content">Home</div>
+		<div class="content">About Us</div>
+		<div class="content">Curriculum</div>
+		<div class="content">Contact Us</div>
+	</section>
+
+	<header class="slideshow-container">
+		<section class="Slides fade">
+	    <img src="icon/menuIcon.png" style="width:100%">
+	  </section>
+		<section class="Slides fade">
+	    <img src="icon/menuIcon.png" style="width:100%">
+	  </section>
+	</header>
+	<script type="text/javascript" src="js/js.js"></script>
+</body>
+</html>
---
+++ js/js.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+   setInterval(showSlides, 5000, slideIndex)
+}, false);
+
+var slideIndex = 1;
+
+function showSlides() {
+  var n = slideIndex;
+  var i;
+  var slides = document.getElementsByClassName("Slides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+  slideIndex++;
+}
